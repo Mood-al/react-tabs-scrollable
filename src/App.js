82842a18f42471f637710900ae842a4fc22f0ed8
Mohammad Aliwi <aliwi299@gmail.com>
2022-04-18 08:06:07 +0300

    init create-react-library@3.1.1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-tabs-scrollable'
+import 'react-tabs-scrollable/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
