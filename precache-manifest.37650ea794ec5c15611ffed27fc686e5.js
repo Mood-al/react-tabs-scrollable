a2e2898556b311dae7301d45fcf93f4bf0485d39
Mohammad Aliwi <aliwi299@gmail.com>
2022-04-18 10:55:35 +0300

    Updates

---
+++ precache-manifest.37650ea794ec5c15611ffed27fc686e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e89f4dd5eeebc01dfb3abf87ccf0e3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "adef1e5b9aa0fcccf747",
+    "url": "./static/css/main.bcaf545c.chunk.css"
+  },
+  {
+    "revision": "883d9a902a16cfc649aa",
+    "url": "./static/js/2.871b0cd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.871b0cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adef1e5b9aa0fcccf747",
+    "url": "./static/js/main.17760a4a.chunk.js"
+  },
+  {
+    "revision": "6ba6d22cc4bf9bcf482c",
+    "url": "./static/js/runtime-main.42a837e6.js"
+  }
+]);
